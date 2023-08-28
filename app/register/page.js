cd0ebbb03ddff443560a4a1cd57672edfd5abe36
Soumajyoti <soumyabwn3@gmail.com>
2023-08-28 19:13:29 +0530

    initially completed

---
+++ app/register/page.js
@@ -0,0 +1,19 @@
+import RegisterForm from '@/components/RegisterForm'
+import React from 'react'
+import { getServerSession } from 'next-auth'
+import { redirect } from 'next/navigation'
+import { authOptions } from '../api/auth/[...nextauth]/route'
+
+const Register = async () => {
+    const session = await getServerSession(authOptions)
+
+    if (session) {
+        redirect('/dashboard')
+    }
+
+    return (
+        <RegisterForm />
+    )
+}
+
+export default Register
